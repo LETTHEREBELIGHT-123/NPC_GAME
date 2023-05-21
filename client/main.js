5dd7a812ed19ed3eba2f9a5cd16ca396b6a3d976
LETTHEREBELIGHT-123 <anditwasgoodg110@gmail.com>
2023-05-22 00:19:57 +0100

    update the server url

--- client/main.js
+++ client/main.js
@@ -71,7 +71,7 @@ function chatstripe(isAi, value, uniqueId) {
     const messageDiv = document.getElementById(uniqueId);
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://npc-game.onrender.com/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
